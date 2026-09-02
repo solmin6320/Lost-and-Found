a10a78981108e1669e4e962868278205a7f76731
solmin6320 <solmin6320@gmail.com>
2026-09-02 23:04:53 +0900

    게시글 요청 DTO 작성 및 게시글 리포 생성

---
+++ src/main/java/com/example/lostandfound/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.lostandfound.repository;
+
+import com.example.lostandfound.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
