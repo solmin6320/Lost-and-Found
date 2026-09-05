3b7a3505514e28c49ba14d0e4c72b3b3f18d041e
solmin6320 <solmin6320@gmail.com>
2026-09-06 00:58:51 +0900

    지원하지 않는 요청 방식 405 예외 처리 추가

--- src/main/java/com/example/lostandfound/exception/ErrorCode.java
+++ src/main/java/com/example/lostandfound/exception/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     // 공통 예외 처리(400, 404 ,500)
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 경로를 찾을 수 없습니다"),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 요청방식입니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다");
 
 
--- src/main/java/com/example/lostandfound/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/lostandfound/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -71,6 +72,17 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(errorCode.getStatus()) // 400
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    // 경로는 맞지만 메서드가 다름에 대한 예외 처리
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException() {
+
+        ErrorCode errorCode = ErrorCode.METHOD_NOT_ALLOWED;
+
+        return ResponseEntity
+                .status(errorCode.getStatus()) // 405
                 .body(ErrorResponse.of(errorCode));
     }
 
